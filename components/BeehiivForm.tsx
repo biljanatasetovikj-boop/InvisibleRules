3cb9566390e512a7c2f35c88ce89c85d5dc7fa3b
Biljana Kuzmanovska Tasetovikj <biljanatasetovikj@gmail.com>
2026-08-14 11:30:08 +0200

    Force beehiiv form to stack full-width with larger touch targets on mobile

--- components/BeehiivForm.tsx
+++ components/BeehiivForm.tsx
@@ -17,5 +17,10 @@ export default function BeehiivForm() {
     container.appendChild(script);
   }, []);
 
-  return <div ref={ref} className="w-full [&_iframe]:!w-full" />;
+  return (
+    <div
+      ref={ref}
+      className="w-full [&_*]:!max-w-full [&_iframe]:!w-full [&_form]:!flex [&_form]:!w-full [&_form]:!flex-col [&_form]:!gap-3 [&_input]:!box-border [&_input]:!w-full [&_input]:!min-h-[54px] [&_input]:!text-[16px] [&_button]:!w-full [&_button]:!min-h-[54px]"
+    />
+  );
 }
